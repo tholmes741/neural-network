9fa2e08b0bc59132be32376bc92979c40a37ebc1
Tommy Holmes <holmesiii.thomas@gmail.com>
2021-04-01 15:47:34 -0400

    update to run network

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
 
 from src.network import Network
+import src.mnist_loader as mnist_loader
+
+import numpy as np
 
 def main():
-    net = Network((1,2,3))
+    net = Network((784, 30, 10))
+    training_data, validation_data, test_data =  mnist_loader.load_data_wrapper()
+    net.SGD(training_data, 30, 10, 3.0, test_data)
 
 if __name__ == '__main__': main()
